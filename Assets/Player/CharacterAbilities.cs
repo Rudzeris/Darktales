96e0b056b4e00288dcb88dbd9e76a5ca79527949
rudzeris <ruzil1atangulov@gmail.com>
2024-11-02 14:34:18 +1000

    add player classes

---
+++ Assets/Player/CharacterAbilities.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterAbilities : MonoBehaviour
+{
+    public void OnAbility1()
+    {
+        Debug.Log("Ability 1 activated!");
+    }
+
+    public void OnAbility2()
+    {
+        Debug.Log("Ability 2 activated!");
+    }
+
+    public void OnAbility3()
+    {
+        Debug.Log("Ability 3 activated!");
+    }
+}
