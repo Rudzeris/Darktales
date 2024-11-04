798f98a39ab170cd86706ca565d58f36c962109c
Rudzeris <ruzil1atangulov@gmail.com>
2024-11-05 01:50:33 +1000

    modify LevelProgress: Saturation, LevelManager: get enemies, enemy.OnDead, enemy.OnHit

--- Assets/Level/LevelManager.cs
+++ Assets/Level/LevelManager.cs
@@ -1,15 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // Класс, который знает информацию про всех и добавляем им event 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private LevelHUD levelHUD;
-
+    [SerializeField] private List<Enemy> enemies;
+    [SerializeField] private uint[] LVLs = new uint[3] { 50, 75, 100 };
     private LevelProgress progress;
+
+    [SerializeField] public int Saturation
+    {
+        get => progress.Saturation;
+        set => progress.Saturation = value;
+    }
+    public int LvlSaturation
+    {
+        get
+        {
+            int lvl = 0;
+            foreach (int i in LVLs)
+                if (Saturation >= i) lvl++;
+            return lvl;
+        }
+    }
 
     private void Awake()
     {
         progress = new LevelProgress();
+        enemies = FindObjectsOfType<Enemy>().ToList();
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.OnDie += EnemyDie;
+            enemy.OnHit += PlayerHitEnemy;
+        }
     }
+
+    private void PlayerHitEnemy(Enemy enemy)
+    {
+        if (!enemy.IsDead)
+        {
+
+        }
+    }
+
+    private void EnemyDie(Enemy enemy)
+    {
+        if(!enemy.IsDead)
+            Saturation += enemy.Score;
+    }
+    
 
 }
--- Assets/Level/LevelProgress.cs
+++ Assets/Level/LevelProgress.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 // Класс для хранения прогресса уровня
 public class LevelProgress
 {
+    private int _saturation;
+    public int Saturation
+    {
+        get => _saturation;
+        set => _saturation = value;
+    }
 }
