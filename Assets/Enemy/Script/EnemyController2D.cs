d997369fbe221d71f2bb1510295150a7fc70fa9f
Rudzeris <ruzil1atangulov@gmail.com>
2024-11-09 02:39:05 +1000

    enemy move towards player and home

--- Assets/Enemy/Script/EnemyController2D.cs
+++ Assets/Enemy/Script/EnemyController2D.cs
@@ -2,5 +2,71 @@ using UnityEngine;
 
 public class EnemyController2D : MonoBehaviour
 {
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float visionRadius = 3f;
+    [SerializeField] private float loseSightRadius = 4f; // Новый радиус для потери игрока
+    private Transform player;
+    private Vector2 initialPosition;
+    private bool movingRight = true;
+    public enum State { Stop, Home, Player };
+    [SerializeField] private State state;
 
+    private void Start()
+    {
+        state = State.Stop;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        float distanceToInitial = Vector2.Distance(transform.position, initialPosition);
+
+        switch (state)
+        {
+            case State.Stop:
+                if (distanceToPlayer < visionRadius)
+                {
+                    state = State.Player;
+                }
+                else if (distanceToInitial > 1f)
+                {
+                    state = State.Home;
+                }
+                break;
+
+            case State.Player:
+                if (distanceToPlayer > loseSightRadius)
+                {
+                    state = State.Home;
+                }
+                else
+                {
+                    MoveTowards(player.position);
+                }
+                break;
+
+            case State.Home:
+                if (distanceToPlayer < visionRadius)
+                {
+                    state = State.Player;
+                }
+                else if (distanceToInitial <= 1f)
+                {
+                    state = State.Stop;
+                }
+                else
+                {
+                    MoveTowards(initialPosition);
+                }
+                break;
+        }
+    }
+
+    private void MoveTowards(Vector2 target)
+    {
+        Vector2 position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = new Vector3(position.x, transform.position.y, transform.position.z);
+    }
 }
