4ef80ec6be1965d2e0a84642c3dd973c0af32e9b
Rudzeris <ruzil1atangulov@gmail.com>
2024-11-09 14:24:51 +1000

    add SaturationCommand, modify UI and levelManager

--- Assets/Level/LevelManager.cs
+++ Assets/Level/LevelManager.cs
@@ -8,35 +8,30 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private LevelHUD levelHUD;
     [SerializeField] private List<Enemy> enemies;
-    [SerializeField] private uint[] LVLs = new uint[3] { 50, 75, 100 };
+    [SerializeField] private int[] LVLs = new int[3] { 50, 75, 100 };
     private LevelProgress progress;
 
     [SerializeField] public int Saturation
     {
         get => progress.Saturation;
-        set => progress.Saturation = value;
-    }
-    public int LvlSaturation
-    {
-        get
-        {
-            int lvl = 0;
-            foreach (int i in LVLs)
-                if (Saturation >= i) lvl++;
-            return lvl;
-        }
+        set { progress.Saturation = value; levelHUD.UpdateSaturation(value); }
     }
 
     private void Awake()
     {
         progress = new LevelProgress();
+        progress.Saturation = 76;
         enemies = FindObjectsOfType<Enemy>().ToList();
         foreach (Enemy enemy in enemies)
         {
             enemy.OnDie += EnemyDie;
         }
+        levelHUD.LVLs = LVLs;
     }
-
+    public void Start()
+    {
+        levelHUD.UpdateSaturation(Saturation);
+    }
     private void EnemyDie(Enemy enemy)
     {
         if (!enemy.IsDead)
---
+++ Assets/UI/Script/Command/SaturationCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.UI
+{
+    public class SaturationCommand : IUICommand
+    {
+        public int Saturation { get;private set; }
+        public SaturationCommand (int saturation)
+        {
+            Saturation = saturation;
+        }
+    }
+}
