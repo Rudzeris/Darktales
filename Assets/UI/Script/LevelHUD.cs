5e1b7302e6662324eda8192a1f3bbad9fd7ffad3
Rudzeris <ruzil1atangulov@gmail.com>
2024-11-11 13:10:18 +1000

    UI: create prefab. Modify Saturation line. Remove button

--- Assets/UI/Script/LevelHUD.cs
+++ Assets/UI/Script/LevelHUD.cs
@@ -8,14 +8,12 @@ using UnityEngine.UI;
 public class LevelHUD : MonoBehaviour
 {
     [Header("UI")]
-    [SerializeField] private TMP_Text scoreText;
-    [SerializeField] private Button reload;
-    [SerializeField] private GameObject winText;
-    [SerializeField] private GameObject[] saturationImages;
-    [SerializeField] private GameObject[] saturationPanels;
+    [SerializeField] private GameObject pauseElement;
+    [SerializeField] private GameObject imageElement;
+    [SerializeField] private GameObject saturationElement;
 
-    private RectTransform[] saturationPanelTransform;
-    private RectTransform[] imageTransforms;
+    private RectTransform saturationTransform;
+    private RectTransform imageTransform;
 
     public int[] Lvls;
 
@@ -29,25 +27,9 @@ public class LevelHUD : MonoBehaviour
     private void Start()
     {
         Time.timeScale = 1f;
-        reload.onClick.AddListener(
-            () => commandQueue.TryEnqueueCommand(new ReloadCommand())
-            );
-        saturationPanelTransform = new RectTransform[saturationPanels.Length];
-        for (int i = 0; i < saturationPanels.Length; i++)
-            saturationPanelTransform[i] = saturationPanels[i].GetComponent<RectTransform>();
 
-        imageTransforms = new RectTransform[saturationImages.Length];
-        for (int i = 0; i < saturationImages.Length; i++)
-            imageTransforms[i] = saturationImages[i].GetComponent<RectTransform>();
-
-        foreach (var i in imageTransforms)
-        {
-            i.offsetMax =
-    new Vector2(
-        (float)0 - saturationPanelTransform[0].sizeDelta.x,
-        i.sizeDelta.y
-);
-        }
+        saturationTransform = saturationElement.GetComponent<RectTransform>();
+        imageTransform = imageElement.GetComponent<RectTransform>();
 
         StartCoroutine(UpdateTask());
     }
@@ -59,7 +41,6 @@ public class LevelHUD : MonoBehaviour
 
     private void OnDestroy()
     {
-        reload.onClick.RemoveAllListeners();
         StopAllCoroutines();
     }
     private IEnumerator UpdateTask()
@@ -70,38 +51,18 @@ public class LevelHUD : MonoBehaviour
                 switch (command)
                 {
                     case SaturationCommand st:
-                        scoreText.text = $"Saturation: {st.Saturation}";
-                        for (int i = 0; i < Lvls.Length; i++)
-                        {
-                            if (st.Saturation < Lvls[i])
-                            {
-                                imageTransforms[i].offsetMax =
-                                new Vector2(
-                                    ( i==0
-? (float)st.Saturation / Lvls[i] * saturationPanelTransform[i].sizeDelta.x - saturationPanelTransform[i].sizeDelta.x
-: (((float)st.Saturation) - Lvls[i - 1]) / (Lvls[i] - Lvls[i-1]) * saturationPanelTransform[i].sizeDelta.x - saturationPanelTransform[i].sizeDelta.x
-                                    ),
-                                    imageTransforms[i].sizeDelta.y
-                            );
-                                break;
-                            }
-                            else
-                            {
-                                imageTransforms[i].offsetMax =
-                                new Vector2(
-                                    0,
-                                    imageTransforms[i].sizeDelta.y
-                            );
-                            }
-                        }
+                        imageTransform.offsetMax =
+                        new Vector2(
+                            -(1-(float)st.Saturation / (float)Lvls[Lvls.Length-1])*saturationTransform.rect.width,
+                            imageTransform.sizeDelta.y
+                    );
+
+
 
                         break;
                     case ReloadCommand:
                         var currentScene = SceneManager.GetActiveScene();
                         yield return SceneManager.LoadSceneAsync(currentScene.name);
-                        break;
-                    case WinCommand:
-                        winText.SetActive(true);
                         break;
                 }
             yield return null;
