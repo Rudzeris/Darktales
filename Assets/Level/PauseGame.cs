f3dfdbc748a0beacd311f93a3bb7317ad519b461
Rudzeris <ruzil1atangulov@gmail.com>
2024-11-11 12:26:50 +1000

    add pause game

---
+++ Assets/Level/PauseGame.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private bool isPaused = false;
+
+    private void OnPause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
